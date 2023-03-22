07b60587c9199abb18803a9a9748b70a187df752
Richard Remer <rich.remer@gmail.com>
2023-03-22 10:11:55 -0700

    add namespaces from RFC

---
+++ src/ns.js
@@ -0,0 +1,7 @@
+export const rdf = "http://www.w3.org/1999/02/22-rdf-syntax-ns#";
+export const ldp = "http://www.w3.org/ns/ldp#";
+export const solid = "http://www.w3.org/ns/solid/terms#";
+export const pim = "http://www.w3.org/ns/pim/space#";
+export const acl = "http://www.w3.org/ns/auth/acl#";
+export const dcterms = "http://purl.org/dc/terms/";
+export const stat = "http://www.w3.org/ns/posix/stat";
